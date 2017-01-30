e0ad461d4460cfae6344fb196f8d73503c8968e6
Guido Gloor <haslo@haslo.ch>
2017-01-30 22:47:26 +0100

    Integrated I18n basics with config

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,12 @@ Bundler.require(*Rails.groups)
 
 module ListJuggleBrowser
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+
+    config.time_zone = 'Bern'
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en]
+    config.i18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
   end
 end
