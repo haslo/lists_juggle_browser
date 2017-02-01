8a132c5b6dc27dd538fc3c143724704a26e7eacb
Guido Gloor <haslo@haslo.ch>
2017-02-02 00:45:19 +0100

    Filter configurator in layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
         large_tournament_multiplier: session[:large_tournament_multiplier].nil? ? true                : session[:large_tournament_multiplier],
         widespread_use_multiplier:   session[:widespread_use_multiplier].nil?   ? true                : session[:widespread_use_multiplier],
         use_ranking_data:            session[:use_ranking_data].nil?            ? 'swiss+elimination' : session[:use_ranking_data],
-        ranking_start:               session[:ranking_start].nil?               ? 10.years.ago        : session[:ranking_start],
+        ranking_start:               session[:ranking_start].nil?               ? 6.months.ago        : session[:ranking_start],
         ranking_end:                 session[:ranking_end].nil?                 ? Time.current        : session[:ranking_end],
     }
   end
