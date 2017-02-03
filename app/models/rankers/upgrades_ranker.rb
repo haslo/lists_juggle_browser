ebb8c9bfaedadfe94f82f019b2311f85e8a56443
Guido Gloor <haslo@haslo.ch>
2017-02-03 02:45:33 +0100

    Fix für upgrade display

--- app/models/rankers/upgrades_ranker.rb
+++ app/models/rankers/upgrades_ranker.rb
@@ -3,7 +3,7 @@ module Rankers
 
     attr_reader :upgrades
 
-    def initialize(ranking_configuration, ship_id: nil, pilot_id: nil, limit: nil, ship_combo_id: nil)
+    def initialize(ranking_configuration, ship_id: nil, pilot_id: nil, limit: nil, upgrade_id: nil)
       start_date = ranking_configuration[:ranking_start]
       end_date   = ranking_configuration[:ranking_end]
       joins      = <<-SQL
@@ -45,13 +45,11 @@ module Rankers
       if pilot_id.present?
         upgrade_relation = upgrade_relation.where('pilots.id = ?', pilot_id)
       end
+      if upgrade_id.present?
+        upgrade_relation = upgrade_relation.where('upgrades.id = ?', upgrade_id)
+      end
       if limit.present?
         upgrade_relation = upgrade_relation.limit(limit)
-      end
-      if ship_combo_id.present?
-        upgrade_relation = upgrade_relation
-                             .joins('inner join ship_combos_ships on ship_combos_ships.ship_id = ships.id')
-                             .where('ship_combos_ships.ship_combo_id = ?', ship_combo_id)
       end
       if ranking_configuration[:tournament_type].present?
         upgrade_relation = upgrade_relation.where('tournaments.tournament_type_id = ?', ranking_configuration[:tournament_type])
