424b050ea1f8b6b4a10b44e217601428486536f4
Guido Gloor <haslo@haslo.ch>
2017-02-04 01:28:53 +0100

    Rebuilt filters to now use URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def ranking_configuration
+    #raise params.inspect
     {
-        large_tournament_multiplier: session[:large_tournament_multiplier].nil? ? true                 : session[:large_tournament_multiplier],
-        widespread_use_multiplier:   session[:widespread_use_multiplier].nil?   ? true                 : session[:widespread_use_multiplier],
-        use_ranking_data:            session[:use_ranking_data].nil?            ? 'all'                : session[:use_ranking_data],
-        ranking_start:               session[:ranking_start].nil?               ? 2.months.ago.to_date : session[:ranking_start],
-        ranking_end:                 session[:ranking_end].nil?                 ? Time.current.to_date : session[:ranking_end],
-        tournament_type:             session[:tournament_type].nil?             ? nil                  : session[:tournament_type],
+        large_tournament_multiplier: params['large_tournament_multiplier'].nil? ? true                 : (params['large_tournament_multiplier'] == 'true'),
+        widespread_use_multiplier:   params['widespread_use_multiplier'].nil?   ? true                 : (params['widespread_use_multiplier'] == 'true'),
+        use_ranking_data:            params['use_ranking_data'].nil?            ? 'all'                : params['use_ranking_data'],
+        ranking_start:               params['ranking_start'].nil?               ? 2.months.ago.to_date : params['ranking_start'],
+        ranking_end:                 params['ranking_end'].nil?                 ? Time.current.to_date : params['ranking_end'],
+        tournament_type:             params['tournament_type'].nil?             ? nil                  : params['tournament_type'],
     }
   end
   helper_method :ranking_configuration
