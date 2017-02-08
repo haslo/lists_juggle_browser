2455367e175d8bfe81eb685dbaa152a5b37cabb8
Guido Gloor <haslo@haslo.ch>
2017-02-08 22:13:48 +0100

    Squadrons now list what they're for

--- app/controllers/squadrons_controller.rb
+++ app/controllers/squadrons_controller.rb
@@ -1,12 +1,32 @@
 class SquadronsController < ApplicationController
 
   def index
-    @view = Rankers::SquadronsRanker.new(ranking_configuration,
-                                         ship_id:       params[:ship_id],
-                                         pilot_id:      params[:pilot_id],
-                                         ship_combo_id: params[:ship_combo_id],
-                                         upgrade_id:    params[:upgrade_id],
-                                         limit:         50)
+    squadrons_ranker = Rankers::SquadronsRanker.new(ranking_configuration,
+                                                    ship_id:       params[:ship_id],
+                                                    pilot_id:      params[:pilot_id],
+                                                    ship_combo_id: params[:ship_combo_id],
+                                                    upgrade_id:    params[:upgrade_id],
+                                                    limit:         50)
+    @view            = OpenStruct.new({
+                                        squadrons:             squadrons_ranker.squadrons,
+                                        rankers:               get_rankers_from_params,
+                                        number_of_tournaments: squadrons_ranker.number_of_tournaments,
+                                        number_of_squadrons:   squadrons_ranker.number_of_squadrons,
+                                      })
+  end
+
+  private
+
+  def get_rankers_from_params
+    [Ship, ShipCombo, Upgrade, Pilot].map do |klass|
+      parameter_name = "#{klass.name.underscore}_id".to_sym
+      if params[parameter_name].present?
+        ranker = "Rankers::#{ShipCombo.name.pluralize}Ranker".constantize
+        [klass, ranker.new(ranking_configuration, { parameter_name => params[parameter_name] })]
+      else
+        nil
+      end
+    end.compact
   end
 
 end
