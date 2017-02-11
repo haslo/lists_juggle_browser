cbc7bbfd02bf35c9dd2d58409da943d613d8a5be
Guido Gloor <haslo@haslo.ch>
2017-02-11 17:00:27 +0100

    Wins and losses much faster, less query, more Ruby

--- app/models/rankers/counter_combo_ranker.rb
+++ app/models/rankers/counter_combo_ranker.rb
@@ -12,28 +12,53 @@ module Rankers
           on squadrons.ship_combo_id = ship_combos.id
         inner join tournaments
           on tournaments.id = squadrons.tournament_id
-        inner join games won_games
-          on won_games.winning_combo_id = ship_combos.id and won_games.tournament_id = tournaments.id
-        inner join games lost_games
-          on lost_games.losing_combo_id = ship_combos.id and lost_games.tournament_id = tournaments.id
-        inner join ship_combos other_combos
-          on won_games.losing_combo_id = other_combos.id and lost_games.winning_combo_id = other_combos.id
       SQL
       counter_combo_query = ShipCombo.where(id: ship_combo_id).joins(joins)
       counter_combo_query = counter_combo_query.where('tournaments.date >= ? and tournaments.date <= ?', start_date, end_date)
       if tournament_type.present?
         counter_combo_query = counter_combo_query.where('tournaments.tournament_type_id = ?', tournament_type)
       end
-      attributes          = {
-        id:                   'ship_combos.id',
-        archetype_name:       'ship_combos.archetype_name',
-        other_id:             'other_combos.id',
-        other_archetype_name: 'other_combos.archetype_name',
-        won_games:            'count(distinct(won_games.id))',
-        lost_games:           'count(distinct(lost_games.id))',
+      won             = won_games(counter_combo_query)
+      lost            = lost_games(counter_combo_query)
+      other_combo_ids = won.map { |w| w[:losing_combo_id] } + lost.map { |l| l[:winning_combo_id] }
+      other_combos    = ShipCombo.where(id: other_combo_ids).includes(:ships)
+      @counter_combos = other_combo_ids.map do |ship_combo_id|
+        ship_combo     = other_combos.detect { |c| c.id == ship_combo_id }
+        wins_against   = won.detect { |w| w[:losing_combo_id] == ship_combo_id }.try(:[], :number_of_wins) || 0
+        losses_against = lost.detect { |w| w[:winning_combo_id] == ship_combo_id }.try(:[], :number_of_losses) || 0
+        {
+          ship_combo:     ship_combo,
+          ships:          ship_combo.ships,
+          games_against:  wins_against + losses_against,
+          wins_against:   wins_against,
+          losses_against: losses_against,
+          win_loss_ratio: wins_against.to_f / (wins_against.to_f + losses_against.to_f),
+        }
+      end.sort_by { |counter| counter[:win_loss_ratio] }
+    end
+
+    def won_games(counter_combo_query)
+      won_games_join = <<-SQL
+        inner join games
+          on games.winning_combo_id = ship_combos.id and games.tournament_id = tournaments.id
+      SQL
+      attributes = {
+        losing_combo_id: 'games.losing_combo_id',
+        number_of_wins:  'count(*)',
       }
-      counter_combo_query = counter_combo_query.group('ship_combos.id, ship_combos.archetype_name, other_combos.id, other_combos.archetype_name')
-      @counter_combos     = ShipCombo.fetch_query(counter_combo_query, attributes)
+      ShipCombo.fetch_query(counter_combo_query.joins(won_games_join).group('games.losing_combo_id'), attributes)
+    end
+
+    def lost_games(counter_combo_query)
+      won_games_join = <<-SQL
+        inner join games
+          on games.losing_combo_id = ship_combos.id and games.tournament_id = tournaments.id
+      SQL
+      attributes = {
+        winning_combo_id: 'games.winning_combo_id',
+        number_of_losses: 'count(*)',
+      }
+      ShipCombo.fetch_query(counter_combo_query.joins(won_games_join).group('games.winning_combo_id'), attributes)
     end
 
   end
