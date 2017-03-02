e217cddfefe285ef65d8932efd30eea57ffd1091
Guido Gloor <haslo@haslo.ch>
2017-03-02 11:35:06 +0100

    More relations

--- app/models/faction.rb
+++ app/models/faction.rb
@@ -1,5 +1,7 @@
 class Faction < ApplicationRecord
 
   has_many :squadrons
+  has_many :pilots
+  has_many :ships, through: :pilots
 
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,6 +2,7 @@ class Ship < ApplicationRecord
   include Concerns::DirectQuery
 
   has_many :pilots
+  has_many :factions, through: :pilots
 
   has_and_belongs_to_many :ship_combos
 
