036a5d77f504e39d0a16193392fe203493e40a2a
Guido Gloor <haslo@haslo.ch>
2017-06-06 22:51:55 +0200

    Update gone for now. Thanks go to the Carolyna Krayts.

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Rails.application.routes.draw do
 
   root 'overviews#index'
 
-  resources :pilots, only: [:index, :show, :update] do
+  resources :pilots, only: [:index, :show] do
     resources :squadrons, only: [:index]
     resource :image, only: [:show]
   end
-  resources :ships, only: [:index, :show, :update] do
+  resources :ships, only: [:index, :show] do
     resources :squadrons, only: [:index]
   end
-  resources :ship_combos, only: [:index, :show, :update] do
+  resources :ship_combos, only: [:index, :show] do
     resources :squadrons, only: [:index]
   end
-  resources :upgrades, only: [:index, :show, :update] do
+  resources :upgrades, only: [:index, :show] do
     resources :squadrons, only: [:index]
     resource :image, only: [:show]
   end
