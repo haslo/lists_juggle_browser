943e55be09a6e09078f4f3b0aff785f23662a0bf
Guido Gloor <haslo@haslo.ch>
2017-07-26 14:55:09 +0200

    Added image export with dom-to-image

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,4 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( turning_test.js )
+Rails.application.config.assets.precompile += %w( dom-to-image.min.js )
