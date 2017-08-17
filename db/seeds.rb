881b2c648288ee60a0656cfd54df0b444cafeb2f
Guido Gloor <haslo@haslo.ch>
2017-08-17 21:52:29 +0200

    Fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-Faction.create!([
-                  { id: 1, name: "Rebel Alliance", xws: 'rebel', is_subfaction: false, primary_faction_id: 1 },
-                  { id: 2, name: "Galactic Empire", xws: 'imperial', is_subfaction: false, primary_faction_id: 2 },
-                  { id: 3, name: "Scum and Villainy", xws: 'scum', is_subfaction: false, primary_faction_id: 3 },
-                  { id: 4, name: "First Order", xws: 'imperial', is_subfaction: true, primary_faction_id: 2 },
-                  { id: 5, name: "Resistance", xws: 'rebel', is_subfaction: true, primary_faction_id: 1 }
-                ])
+[
+  { id: 1, name: "Rebel Alliance", xws: 'rebel', is_subfaction: false, primary_faction_id: 1 },
+  { id: 2, name: "Galactic Empire", xws: 'imperial', is_subfaction: false, primary_faction_id: 2 },
+  { id: 3, name: "Scum and Villainy", xws: 'scum', is_subfaction: false, primary_faction_id: 3 },
+  { id: 4, name: "First Order", xws: 'imperial', is_subfaction: true, primary_faction_id: 2 },
+  { id: 5, name: "Resistance", xws: 'rebel', is_subfaction: true, primary_faction_id: 1 }
+].each do |attributes|
+  f = Faction.new(attributes)
+  f.save(validate: false)
+end
