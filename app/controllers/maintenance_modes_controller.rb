b994b28f5d500007ab6719efc5970c715244f36c
Guido Gloor <haslo@haslo.ch>
2017-08-18 08:50:57 +0200

    Never show maintenance mode page outside maintenance mode

--- app/controllers/maintenance_modes_controller.rb
+++ app/controllers/maintenance_modes_controller.rb
@@ -1,9 +1,18 @@
 class MaintenanceModesController < ApplicationController
 
   skip_before_action :check_maintenance_mode
+  before_action :check_no_maintenance_mode
 
   def show
     # just show the view
+  end
+
+  private
+
+  def check_no_maintenance_mode
+    unless KeyValueStoreRecord.get('maintenance')
+      redirect_to root_path
+    end
   end
 
 end
