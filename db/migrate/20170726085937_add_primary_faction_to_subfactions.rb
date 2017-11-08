7113aa9b333f2bb939ad61f2d2df7a3249e6f12d
Guido Gloor <haslo@haslo.ch>
2017-11-08 20:52:29 +0100

    Migration fix

--- db/migrate/20170726085937_add_primary_faction_to_subfactions.rb
+++ db/migrate/20170726085937_add_primary_faction_to_subfactions.rb
@@ -7,11 +7,11 @@ class AddPrimaryFactionToSubfactions < ActiveRecord::Migration[5.1]
     reversible do |dir|
       dir.up do
         execute('update factions set primary_faction_id = id')
-        imperial_id = select_rows("select id from factions where name = 'Galactic Empire'")[0][0]
+        imperial_id = select_rows("select id from factions where name = 'Galactic Empire'").dig(0, 0)
         if imperial_id.present?
           execute("update factions set primary_faction_id = #{imperial_id} where name = 'First Order'")
         end
-        rebels_id = select_rows("select id from factions where name = 'Rebel Alliance'")[0][0]
+        rebels_id = select_rows("select id from factions where name = 'Rebel Alliance'").dig(0, 0)
         if rebels_id.present?
           execute("update factions set primary_faction_id = #{rebels_id} where name = 'Resistance'")
         end
