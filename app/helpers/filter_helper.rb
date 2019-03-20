697d6c6f8f523ce60002ff874345d28c47f9f87f
mwhited <matthew.whited@gmail.com>
2019-03-20 14:07:57 -0700

    added the Wave 3 date

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -11,6 +11,7 @@ module FilterHelper
       [I18n.t('shared.filter_configurator.dates.wave_2'), Date.new(2018, 12,13)],
       [I18n.t('shared.filter_configurator.dates.jan_19_points_update'), Date.new(2019, 1, 28)],
       [I18n.t('shared.filter_configurator.dates.upsilon_nerf'), Date.new(2019, 2, 28)],
+      [I18n.t('shared.filter_configurator.dates.wave_3'), Date.new(2019, 3, 20)],
       [I18n.t('shared.filter_configurator.dates.today'), Date.today],
     ]
   end
