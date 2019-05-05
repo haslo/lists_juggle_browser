629082ab72f36889512ba1be59bc4ee3d132e62a
Alex Raubach <antigrapist@gmail.com>
2019-05-05 00:08:21 -0400

    Add profile for heroku

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 rails_env = ENV['RAILS_ENV'] || 'development'
 environment rails_env
 
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 4 }.to_i
 threads threads_count, threads_count
 port        ENV.fetch('PORT') { 3001 }
 workers ENV.fetch('WEB_CONCURRENCY') { 1 }
